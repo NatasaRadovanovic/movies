cceddda8990f6646a1a9236652fddb673cccc47e
NatasaRadovanovic <natasar.radovanovic@gmail.com>
2018-07-17 20:10:00 +0200

    zadatak5

---
+++ resources/views/movies/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+    <p>{{ $movie->title }}<p>
+    <p>{{ $movie->genre }}<p>
+    <p>{{ $movie->director }}<p>
+    <p>{{ $movie->produstion_year }}<p>
+    <p>{{ $movie->storyline }}</p>
+    <hr></hr> 
+@endsection  
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/movies', 'MoviesController@index');
-//Route::get('/movies/{id}', 'MoviesController$show');
+Route::get('/movies/{id}', 'MoviesController@show');
