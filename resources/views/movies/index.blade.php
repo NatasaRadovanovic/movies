13019e2c9df50a77bc5bdff2e2c50ea738c945b3
NatasaRadovanovic <natasar.radovanovic@gmail.com>
2018-07-17 20:01:48 +0200

    zadatak4

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,19 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('content')
+    
     @foreach($movies as $movie)
         <p><a href='/movies/{{$movie->id}}'>{{ $movie->title }}</a><p>
         <p>{{ $movie->storyline }}</p>
         <hr></hr>
-    @endforeach    
+    @endforeach  
+@endsection  
     
 
     
-</body>
-</html>
